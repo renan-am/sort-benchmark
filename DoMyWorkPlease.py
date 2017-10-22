5a59a982b3edc8408c64df9e825abda780764683
Marcelo Vilela <marcelo.filho.cps@gmail.com>
2017-10-22 19:56:49 -0200

    some tests in python to print the graph

--- DoMyWorkPlease.py
+++ DoMyWorkPlease.py
@@ -2,8 +2,9 @@ import os
 
 class resultForSize:
     def __init__(self):
+        self.sortName = ''
         self.inputSize = 0
-        self.avarageTime = 0
+        self.averageTime = 0
 
 os.chdir('outputs/')
 i = 0
@@ -17,11 +18,25 @@ print(sortArchives)
 
 
 
-results =
+results = [];
+accumulator = 0;
+counter = 0;
+
 
 for x in range(0,i):
     file_object = open(sortArchives.pop(),"r")
     for line in file_object:
         if line[0] is '*':
             newstr = line.replace("*", "")
-            print(newstr)
+            size = int(newstr)
+        elif line[0] is '\n':
+            new = resultForSize()
+            new.name = file_object
+            new.averageTime = accumulator/counter
+            new.inputSize = size
+            result.append(new)
+            accumulator = 0
+            counter = 0
+        else:
+            accumulator += float(line)
+            counter++
