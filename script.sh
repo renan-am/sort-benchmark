b7a9241cf00a6e3360f074cedc11e9ee65810be7
Renan <recam94@gmail.com>
2017-10-22 13:32:02 -0200

    fix script

--- script.sh
+++ script.sh
@@ -2,11 +2,9 @@ tamanhos="5000
 10000 
 25000
 50000 
-10000 
+100000 
 250000 
 500000"
-
-inputNames=" "
 
 for tam in $tamanhos
 do
@@ -14,24 +12,26 @@ do
 			gerador="./gerador/gerador <$tam 1000000"
 			mkdir -p "inputs/$tam/"
 			echo "$gerador" >> "inputs/$tam/input$i"
-			inputNames+="input$i "
 		done
 done
 
 for tam in $tamanhos
 do
 	echo $tam
+	echo
 	for sortPath in sorts/*
 	do
 		sort=$(basename $sortPath)
 		echo $sort
 		mkdir -p "outputs/"
 		echo "$tam" >> "outputs/$sort.out"
-			for input in $inputNames
+			for inputPath in inputs/$tam/*
 			do	
+				input=$(basename $inputPath)
 				var=$(./sorts/$sort <"inputs/$tam/$input")
 				echo "$var" >> "outputs/$sort.out"
 			done
 	echo " " >> "outputs/$sort.out"
 	done
+	echo
 done
