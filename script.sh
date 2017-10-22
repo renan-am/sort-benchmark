3b0fda9135feb8537f8a4c74113ea32e6a4b38a3
Renan <recam94@gmail.com>
2017-10-22 17:07:36 -0200

    updates

--- script.sh
+++ script.sh
@@ -1,50 +1,32 @@
 #!/bin/bash
 
-# Executes command with a timeout
-# Params:
-#   $1 timeout in seconds
-#   $2 command
-# Returns 1 if timed out 0 otherwise
-timeouts() {
-
-    time=$1
-
-    # start the command in a subshell to avoid problem with pipes
-    # (spawn accepts one command)
-    command="/bin/sh -c \"$2\""
-
-    expect -c "set echo \"-noecho\"; set timeout $time; spawn -noecho $command; expect timeout { exit 1 } eof { exit 0 }"    
-
-    if [ $? = 1 ] ; then
-        echo "Timeout after ${time} seconds"
-    fi
-
-}
-
 tamanhos="5000
 10000
 25000
 50000
 100000
 250000
-500000"
+500000
+1000000"
 
 for tam in $tamanhos
 do
 	for ((i = 0; i<10; i++)); do
+			mkdir -p "inputs/$tam/"
 			path="/gerador/input"
 			echo "$tam" > gerador/input
 			echo "1000000" >> gerador/input
 			gerador=$(./gerador/gerador <gerador/input > inputs/$tam/input$i)
-			mkdir -p "inputs/$tam/"
 			$gerador
 		done
+		echo gerou $tam
 		echo -n > gerador/input
 done
 
 for sortPath in sorts/*
 do
 	sort=$(basename $sortPath)
+	mkdir -p outputs
 	echo $sort
 	echo
 	echo -n > "outputs/$sort.out"
@@ -66,6 +48,7 @@ do
 				if [ $? -eq 124 ]; 
 					then 
 						flag=1
+						echo TIMEOUT
 						break; 
 				fi
 
