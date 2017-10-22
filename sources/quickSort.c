22dc6997bef1e14441bc948bf35226161d50d1e1
Marcelo Vilela <marcelo.filho.cps@gmail.com>
2017-10-22 13:13:29 -0200

    adicionado o quickSort, randomquickSort, quickSort, selectionSort e shellSort

---
+++ sources/quickSort.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<unistd.h>
+#define BILLION 1E9
+
+void swap(int* a, int* b);
+int partition (int arr[], int low, int high);
+void quickSort(int arr[], int low, int high);
+
+int main(){
+
+	struct timespec requestStart, requestEnd;
+	int *vector = NULL;
+	int size, aux;
+
+	scanf("%d",&size);
+
+	vector = malloc(size*sizeof(int));
+
+	for(int i = 0; i < size; i++){
+		scanf("%d",&aux);
+		vector[i] = aux;
+	}
+
+	clock_gettime(CLOCK_REALTIME, &requestStart);
+
+	quickSort(vector,0 , size -1);
+
+  clock_gettime(CLOCK_REALTIME, &requestEnd);
+
+	double accum = ( requestEnd.tv_sec - requestStart.tv_sec )
+		+ ( requestEnd.tv_nsec - requestStart.tv_nsec )
+		/ BILLION;
+
+		printf( "%.9lf\n", accum );
+
+}
+
+
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+
+    for (int j = low; j <= high- 1; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot)
+        {
+            i++;    // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+/* The main function that implements QuickSort
+ arr[] --> Array to be sorted,
+  low  --> Starting index,
+  high  --> Ending index */
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+           at right place */
+        int pi = partition(arr, low, high);
+
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
