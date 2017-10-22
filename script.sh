1b41f2cb56c0c087964d1c09cb8a229a86a73b73
Renan <recam94@gmail.com>
2017-10-22 11:50:25 -0200

    inicio

---
+++ script.sh
@@ -0,0 +1,36 @@
+tamanhos="5000 
+10000 
+25000
+50000 
+10000 
+250000 
+500000"
+
+inputNames=""
+
+sortNames="quickSort
+"
+
+for tam in $tamanhos
+do
+	for ((i = 0; i<10; i++)); do
+			gerador="./gerador <$tam 1000000"
+			mkdir -p "inputs/$tam/"
+			echo "$gerador" >> "inputs/$tam/input$i"
+			inputNames+=" input$i"
+		done
+done
+
+for tam in $tamanhos
+do
+	for sort in $sortNames
+	do
+		echo "$tam" >> "outputs/$sort.out"
+		for input in $inputNames
+		do	
+			var=$(./"$sort" <"inputs/$tam/$input")
+			echo "$var" >> "outputs/$sort.out"
+		done
+	echo " " >> "outputs/$sort.out"
+	done
+done
