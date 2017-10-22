b9be9e0d1c110baf598dc0e6103c612229bf7d7c
Marcelo Vilela <marcelo.filho.cps@gmail.com>
2017-10-22 15:25:28 -0200

    compilar fixed, to compile parallel versions of sorts

--- compilar.sh
+++ compilar.sh
@@ -3,5 +3,13 @@ do
 	file=$(basename $filePath)
 	name=$(echo $file | cut -f 1 -d '.')
 	$(gcc sources/$name.c -o sorts/$name)
-	
+
+done
+
+for filePath in sources/*.pll.cpp
+do
+	file=$(basename $filePath)
+	name=$(echo $file | cut -f 1 -d '.')
+	$(g++ sources/$name.pll.cpp -o sorts/$name -pthread -std=c++11)
+
 done
